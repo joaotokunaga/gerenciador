78f4f4153755afa4732daf4434eb805917f890f8
João Tokunaga <joaotokunaga@gmail.com>
2022-05-12 14:40:21 -0300

    Roteamento, passando dados via hierarquia de componentes e lendo parâmetros de rotas.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+	selector: 'app-menu',
+	templateUrl: './menu.component.html',
+	styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+	active: String = 'ciclo'
 
-  ngOnInit(): void {
-  }
+	constructor(private router: Router) {
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationStart) {
+				if (event.url == '/') this.active = 'ciclo'
+				if (event.url == '/disciplinas') this.active = 'disciplinas'
+				if (event.url.split('?')[0] == '/rotas-com-parametro') this.active = 'rotas'
+			}
+		})
+	}
+
+	ngOnInit(): void {
+	}
 
 }
